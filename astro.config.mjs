f40973c37c7d87b68ebd9eb92440286922af9b36
t4traw <t4traw@gmail.com>
2023-07-05 11:12:58 +0900

    公式のprefetchに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), prefetch()]
 });
