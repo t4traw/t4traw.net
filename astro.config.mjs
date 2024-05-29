60a6c20b2a626b8bea3a3f60b380971f5854e9a7
t4traw <t4traw@gmail.com>
2024-05-29 22:55:41 +0900

    環境周りをアップデート

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
@@ -8,5 +7,5 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), prefetch()]
+  integrations: [mdx(), prefetch()],
 });
